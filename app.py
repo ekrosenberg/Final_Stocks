44a038b8b7b06866257ab3ed23645da62a3306f1
ekrosenberg <ekrosenb@asu.edu>
2025-02-19 19:51:12 -0700

    Added app.py, rebuilt structure of templates folder, setup venv, added routes as well

--- app.py
+++ app.py
@@ -1,2 +1,24 @@
-print("Testing 123, testing 456")
-print("More testing...")
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap5
+
+app = Flask(__name__)
+bootstrap = Bootstrap5(app)
+
+@app.route('/')
+def base():
+    return render_template('base.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/stock_management')
+def stock_management():
+    return render_template('stock_management.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)

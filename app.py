a3660bca15654e553a39263e09ae981eab23c918
Zak-Young1 <zyoung5@asu.edu>
2025-02-26 17:22:03 -0700

    added most of the login app routes and extensions
    added most of the needed extensions and app routes, still need to test and adjust code, but marking progress for now

--- app.py
+++ app.py
@@ -2,19 +2,73 @@
 
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap5
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
+
+# config for db for users
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@localhost/flask_project' # make sure to put your own sql database logins
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'your-secret-key'
+
+# extensions for the database and user system
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+# user model creation for db and login
+class Users(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(250), unique=True, nullable=False)
+    password = db.Column(db.String(250), nullable=False)
+    role = db.Column(db.String(50), default="user", nullable=False)  # "user" only, manually created "admin" accounts
+
+# creates table for the db
+with app.app_context():
+    db.create_all()
+
+# user loader to retrieve id numbers
+@login_manager.user_loader
+def load_user(user_id):
+    return Users.query.get(int(user_id))
 
 #Shared login HTML pages between users and admins
 
 @app.route('/login')
 def login():
-    return render_template('login.html')              
+    if request.method == "POST":
+        user = Users.query.filter_by(username=request.form.get("username")).first()
+        if user and user.password == request.form.get("password"):
+            login_user(user)
+            return redirect(url_for("login"))
+    return render_template("login.html")            
 
 @app.route('/')
-def base():
-    return render_template('user_dashboard.html')    #---> ZAK REPLACE user_dashboard.html with login.html once login.html is functional
+@login_required  # Restricts access to users not signed in
+def home():
+    return render_template("login.html")
+
+@app.route('/sign_up', methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        user = Users(
+            username=request.form.get("username"),
+            password=request.form.get("password"),  # password unhashed for now, as still adjusting settings and code
+            role="user"  # user is default
+        )
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for("login"))
+    return render_template("sign_up.html")
+
+# logout route added
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("login"))
 
 #User-exclusive app route pages
 

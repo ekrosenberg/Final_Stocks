5caa5ad808e6a79a2735365afcf77423cffdfd14
Zak-Young1 <zyoung5@asu.edu>
2025-04-17 14:12:10 -0700

    Final Final Fix for the App.py
    Final AWS Fix

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ bootstrap = Bootstrap5(app)
 # Database Config
 app.config['SECRET_KEY'] = 'your-secret-key'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:ZINaRQfPPDqHAA0OP62M@my-rds-instance.cubaeu46y77l.us-east-1.rds.amazonaws.com:3306/AWS' # Edit SQL login for local machine
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:ZINaRQfPPDqHAA0OP62M@my-rds-instance.cubaeu46y77l.us-east-1.rds.amazonaws.com:3306/flask_project' # Edit SQL login for local machine
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)

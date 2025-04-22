498b531e4440d435a45bc04af87201c47b85f16a
Zak-Young1 <zyoung5@asu.edu>
2025-04-22 12:21:17 -0700

    Forgot Import

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # Python Imports
 
-from flask import Flask, render_template, session
+from flask import Flask, render_template, session, get_flashed_messages
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
